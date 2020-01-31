3802d468afbe268c724022959b4f1050ea1439e9
Felipe <Mendonca>
2020-01-31 11:25:43 +0100

    added dummy operator

---
+++ exercise_launch/pipelines.py
@@ -0,0 +1,24 @@
+import airflow
+
+from airflow.models import DAG
+from airflow.operators import DummyOperator
+
+args = {
+    "schedule_interval": "0 0/1 * 1/1 * ? *",
+}
+
+dag = DAG(
+    dag_id="test_scheduled_interval",
+    default_args=args,
+    description="DAG just scheduling the next step",
+)
+
+with dag:
+    t1 = DummyOperator(task_id='t1')
+    t2 = DummyOperator(task_id='t2')
+    t3 = DummyOperator(task_id='t3')
+    t4 = DummyOperator(task_id='t4')
+    t5 = DummyOperator(task_id='t5')
+
+
+t1 >> t2 >> [t3, t4] >> t5
